0e3590fbd2fe3e5966d0b20707f74ff028a3e550
WillG-git <19wgyamfi@haz5d.com>
2024-09-11 18:43:39 +0100

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,19 @@
+let myVariable;
+//semi colons at the end of sentences indicated where
+//the sentence ends
+myVariable = "Bob";
+// or you can do
+let myVariable = "Bob";
+//Retrieve it by calling variable name
+myVariable;
+// u can change this value later in the code
+myVariable = 'Steve';
+//js also has strings, booleans but also has
+//numbers python equivalent of integer
+//array ie let myVariable = [1, 'bob' 2, 'Steve']
+//arrays let you store multiple values in a single ref
+let myVariable = 3;
+myVariable === 4;
+//This performs a test to see if two values are equal 
+// and of the same data type
+
